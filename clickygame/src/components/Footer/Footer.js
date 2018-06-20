e24f08de22753f3050d63de0f30eff776c4bf948
Tia Sirrine <tiasirrine@outlook.com>
2018-06-20 14:21:10 -0700

    deploying to gh pages

--- clickygame/src/components/Footer/Footer.js
+++ clickygame/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = props => (
-  <div className="footer">
+  <div className="footer d-inline-flex justify-content-center bd-highlight">
     <div className="row">
       <div className="col-sm 4" />
       <div className="col-sm 4">Made By Tia Sirrine</div>
--- clickygame/src/components/Navbar/Navbar.js
+++ clickygame/src/components/Navbar/Navbar.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Navbar.css';
 
 const Navbar = props => (
-  <div className="navbar">
+  <div className="nav d-inline-flex justify-content-center bd-highlight">
     <div className="row">
       <div className="col-sm 4">Clicky Game</div>
       <div className="col-sm 4">Click on Image</div>
-      <div className="col-sm 4">
+      <div className="col-sm 4 align-left">
         Score: {props.score} | High Score: {props.highscore}
       </div>
     </div>
